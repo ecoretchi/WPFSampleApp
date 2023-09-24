abba68821bea156198fb4fad7b59a5bc2668b85a
Евгений Корецкий <mcgeenerman@gmail.com>
2023-09-24 19:09:50 +0300

    Update ViewModel.cs

--- TestDataContextBindings/ViewModels/ViewModel.cs
+++ TestDataContextBindings/ViewModels/ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace TestDataContextBindings.ViewModels
 {
+    using System;
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.Windows.Controls;
@@ -23,6 +24,9 @@
 
     internal class ViewModel : ViewModelBase, INotifyPropertyChanged
     {
+
+        public string BindingInfo { get; set; } = $"This view binded to ViewModel";
+
         Dictionary<PageNum, MyViewPanel> pages = new ()
         {
             { PageNum.Page1, new MyViewPanel { uc = new UserControl1(), vm = new ViewModel1() } },
@@ -37,6 +41,15 @@
         public RelayCommand OnM2Button => new RelayCommand(() => SetPage(PageNum.Page2));
 
         public RelayCommand OnM3Button => new RelayCommand(() => SetPage(PageNum.Page3));
+
+        public RelayCommand<object> DoSome => new RelayCommand<object>(state =>
+        {
+            var jobs = state as IJobs;
+            var str = jobs?.DoJob();
+            BindingInfo = $"Do some ... {str}";
+            RaisePropertyChanged(nameof(BindingInfo));
+        });
+
 
         public ViewModel()
         {
