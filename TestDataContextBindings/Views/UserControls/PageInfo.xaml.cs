f5d1e3aaa68e607ffe4d348648a606fe778adb18
Евгений Корецкий <mcgeenerman@gmail.com>
2023-09-25 03:01:55 +0300

    Added link uri to sample question and open default brawser

--- TestDataContextBindings/Views/UserControls/PageInfo.xaml.cs
+++ TestDataContextBindings/Views/UserControls/PageInfo.xaml.cs
@@ -1,8 +1,10 @@
 ﻿namespace TestDataContextBindings.Views.UserControls
 {
     using System.Diagnostics;
+    using System.Runtime.InteropServices;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Navigation;
     using TestDataContextBindings.ViewModels;
 
     public partial class PageInfo : UserControl
@@ -10,6 +12,40 @@
         public PageInfo()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {            
+            OpenUrl(e.Uri.ToString());
+        }
+
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
     }
 }
