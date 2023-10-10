edef520a7844bf28b8edc5ab1bf93879672bfd9c
Евгений Корецкий <mcgeenerman@gmail.com>
2023-10-10 04:18:41 +0300

    Test WpfAppEventButtonClick

--- TestDataContextBindings/App.xaml.cs
+++ TestDataContextBindings/App.xaml.cs
@@ -8,10 +8,32 @@ using System.Windows;
 
 namespace TestDataContextBindings
 {
+
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+
+
+        Task someTask = new Task(() =>
+        {
+            Console.WriteLine("Do test task");
+        });
+
+        public App()
+        {
+            someTask.Start();
+            someTask.Wait();
+            someTask.Dispose();
+            someTask.Start();
+            someTask.Wait();
+            someTask.Start();
+            someTask.Wait();
+
+        }
+
+
     }
 }
